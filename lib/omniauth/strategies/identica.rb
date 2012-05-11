62bf578661f987dd7954da1801cfd27fa4f34136
Jonas Lundqvist <lundqvist.j@gmail.com>
2012-05-11 22:20:30 +0200

    use macros instead of initializer

--- lib/omniauth/strategies/identica.rb
+++ lib/omniauth/strategies/identica.rb
@@ -12,13 +12,10 @@ module OmniAuth
     #    use OmniAuth::Strategies::Identica, 'consumerkey', 'consumersecret'
     #
     class Identica < OmniAuth::Strategies::OAuth
-      def initialize(app, consumer_key, consumer_secret, options = {})
-        super(app, :identica, consumer_key, consumer_secret,
-                {:site => 'http://identi.ca',
-                :request_token_path => "/api/oauth/request_token",
-                :access_token_path  => "/api/oauth/access_token",
-                :authorize_path     => "/api/oauth/authorize"}, options)
-      end
+      option :name, 'identica'
+      option :client_options, {:authorize_path => "/api/oauth/authorize",
+                               :request_token_path => "/api/oauth/request_token",
+                               :site => 'http://identi.ca'}
 
       def auth_hash
         OmniAuth::Utils.deep_merge(super, {
